1fe663c140b2f320be46dac31613f7d0d9d55b47
Charlie Li <yl11475@nyu.edu>
2025-10-11 17:26:27 -0400

    Remove legacy Node.js backend implementation

--- index.js
+++
@@ -1,7 +0,0 @@
-const app = require("./app"); // Import app.js
-const PORT = 3000;
-
-// Start the Server
-app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
-});
